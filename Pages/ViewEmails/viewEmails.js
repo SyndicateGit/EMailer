7aa3159c49b1a52bfe7f1ebf0a13a9a188fea1d1
Raymond Zeng <shilei.zeng98@outlook.com>
2024-07-12 21:42:46 -0400

    Display mock email data

--- Pages/ViewEmails/viewEmails.js
+++ Pages/ViewEmails/viewEmails.js
@@ -27,3 +27,23 @@ const emailList = [
     time: '09:30'
   },
 ];
+
+function generateEmailCards(emails){
+  const emailsDiv = document.getElementById('emails');
+  const emailCards = emails.map(email => {
+    return `
+      <div class="email">
+        <h2>${email.to_email}</h2>
+        <h3>${email.email_subject}</h3>
+        <p>${email.email_body}</p>
+        <div class='flex justify-between'>
+          <p>${email.date}</p>
+          <p>${email.time}</p>
+        </div>
+      </div>
+    `;
+  });
+  emailsDiv.innerHTML = emailCards.join('');
+}
+
+generateEmailCards(emailList);
