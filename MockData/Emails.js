f6ff0929c09aa6b63080ad80f1da2d835681fb7c
Raymond Zeng <shilei.zeng98@outlook.com>
2024-07-12 21:17:35 -0400

    Added mock email data

---
+++ MockData/Emails.js
@@ -0,0 +1,29 @@
+const emailList = [
+  {
+    _id: '1',
+    to_email: 'recipient1@example.com',
+    from_email: 'sender1@example.com',
+    email_body: 'Hello, this is the body of the email 1.',
+    email_subject: 'Subject 1',
+    date: '2024-07-12',
+    time: '08:30'
+  },
+  {
+    _id: '2',
+    to_email: 'recipient2@example.com',
+    from_email: 'sender2@example.com',
+    email_body: 'Hello, this is the body of the email 2.',
+    email_subject: 'Subject 2',
+    date: '2024-07-12',
+    time: '09:00'
+  },
+  {
+    _id: '3',
+    to_email: 'recipient3@example.com',
+    from_email: 'sender3@example.com',
+    email_body: 'Hello, this is the body of the email 3.',
+    email_subject: 'Subject 3',
+    date: '2024-07-12',
+    time: '09:30'
+  },
+];
--- Pages/models/EmailModel.js
+++
@@ -1,9 +0,0 @@
-{
-  _id: '';
-  to_email: '';
-  from_email: '';
-  email_body: '';
-  email_subject: '';
-  date: '';
-  time: '';
-}
