665d7303fa91008d8718d9de53bd3e1e6326f34b
Raymond Zeng <shilei.zeng98@outlook.com>
2024-07-19 18:03:23 -0400

    Show edit only for draft email and show draft tag

--- Pages/ViewEmails/viewEmails.js
+++ Pages/ViewEmails/viewEmails.js
@@ -1,32 +1,39 @@
 const emailList = [
   {
     _id: '1',
+    user: '1',
     to_email: 'recipient1@example.com',
     from_email: 'sender1@example.com',
     email_body: 'Hello, this is the body of the email 1.',
     email_subject: 'Subject 1',
+    is_draft: false,
     date: '2024-07-12',
     time: '08:30'
   },
   {
     _id: '2',
+    user: '1',
     to_email: 'recipient2@example.com',
     from_email: 'sender1@example.com',
     email_body: 'Hello, this is the body of the email 2.',
     email_subject: 'Subject 2',
+    is_draft: true,
     date: '2024-07-12',
     time: '09:00'
   },
   {
     _id: '3',
+    user: '1',
     to_email: 'recipient3@example.com',
     from_email: 'sender1@example.com',
     email_body: 'Hello, this is the body of the email 3.',
     email_subject: 'Subject 3',
     date: '2024-07-12',
+    is_draft: false,
     time: '09:30'
   },
 ];
+
 
 function generateEmailCards(emails){
   const emailsDiv = document.getElementById('emails');
@@ -36,13 +43,14 @@ function generateEmailCards(emails){
         <div class='flex justify-between'>
           <h3>${email.to_email}</h3>
           <div class="flex justify-between">
-            <button id='${"edit-"+ email._id}'>Edit</button>
+            <button class='${"visible-"+ email.is_draft}' id='${"edit-"+ email._id}'>Edit</button>
             <button id='${"delete-"+ email._id}'>Delete</button>
           </div>
         </div>
         
         
         <h4>${email.email_subject}</h4>
+        <h5 class='${"visible-"+ email.is_draft}'>Draft</h5>
         <p>${email.email_body}</p>
         <div class='flex flex-1 justify-between items-end'>
           <p>${email.date}</p>
