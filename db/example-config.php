73876c5e5b914d31990c0cf091c38f8f09d7e48c
Dave- blanchard <DBlanchard88@github.com>
2024-07-19 15:43:01 -0400

    fixing readme
    Signed-off-by: Dave- blanchard <DBlanchard88@github.com>

--- db/example-config.php
+++ db/example-config.php
@@ -2,10 +2,10 @@
 $CFG = new stdClass();
 
 # Replace the following URL with your site's URL...
-$CFG->base_url = 'website URL';
+$CFG->base_url = 'your website URL';
 
 # Site-wide password salt...
-$CFG->site_wide_password_salt = 'ask me for the salt';
+$CFG->site_wide_password_salt = 'password salt';
 
 # Set a "global"  session timeout...
 $CFG->session_timeout = 60*10; // in seconds
@@ -13,15 +13,15 @@ $CFG->session_timeout = 60*10; // in seconds
 # Database information...
 $CFG->dbtype = 'mysql';
 $CFG->dbhost = 'localhost';
-$CFG->dbname = 'ASK ME FOR DB NAME';
-$CFG->dbuser = 'ASK ME FOR DB NAME';
-$CFG->dbpass = 'ASK ME FOR DB PASSWORD';
+$CFG->dbname = 'our_database_name';
+$CFG->dbuser = 'our_username';
+$CFG->dbpass = 'your_password';
 
 # Special database "admin" security settings...
 $CFG->db_admin_permit_create_drop = TRUE;
-$CFG->db_admin_only_allow_ip = 'LETS TALK ABOUT IT';
+$CFG->db_admin_only_allow_ip = 'your_ip_address (while connected to GlobalProtect VPN)';
 
 # e.g., Special email support address...
-$CFG->emailaddr_support = 'MY EMAIL';
+$CFG->emailaddr_support = 'your@adminEmail.com';
 
 ?>
