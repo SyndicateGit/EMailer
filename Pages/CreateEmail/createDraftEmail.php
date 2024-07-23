06de5970328e0b1f419247cff345917d00415c2c
Raymond Zeng <shilei.zeng98@outlook.com>
2024-07-23 08:20:02 -0400

    Have create email page call different form action based on button clicked

--- Pages/CreateEmail/createDraftEmail.php
+++ Pages/CreateEmail/createDraftEmail.php
@@ -18,7 +18,7 @@ $dbEmail = new dbemail();
 $user = $dbUser->lookup($_SESSION['user_id']);
 $date = date('Y-m-d');
 $time = date('H:i:s');
-$draft = 0; // Not a draft
+$draft = 1; // Draft
 
 $dbEmail->insert($user['user'], $to_email, $from_email, $email_body, $email_subject, $draft, $date, $time);
 
